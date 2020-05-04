0030169b570e6f1f1b83d89cf9c7545b07109868
Steven Chase <stevenchase@Stevens-MacBook-Pro.local>
2020-05-04 17:38:06 -0400

    Setup local imports

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -1,19 +1,13 @@
-17 lines (10 sloc)  358 Bytes
-  
-
-
-
 def enlarge(n):
     return int(n) * 100
-
 
 if __name__ == "__main__":
     # only run the following code
     # when we run this script from the command-line
     # otherwise don't invoke this code (for example when importing from another file)
 
-    x = 5
+    x=5
     print(enlarge(x))
 
-    z = input("Please choose a number to enlarge: ")
+    z= input('Please choose a number to enlarge:')
     print(enlarge(int(z)))
--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -1,16 +1,13 @@
 import pandas
 
-#import my_mod
-#from my_mod import enlarge # works
 from my_lambdata.my_mod import enlarge
 
-print("HELLO WORLD")
+print("Hello World")
 
 print(2+2)
 
-df = pandas.DataFrame({"a":[1,2,3],"b":[4,5,6]})
+df = pandas.DataFrame({"a":[1,2,3], "b":[4,5,6]})
 print(df.head())
-
 
 y = 9
 print(enlarge(y))
