c86f0541f47782e4273d2909b3beeff8b9a447dd
Steven Chase <stevenchase@Stevens-MacBook-Pro.local>
2020-05-05 22:41:04 -0400

    added main code block. Updataed script to include my mod

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -66,3 +66,11 @@ def list_to_column(input_list, input_df):
     input_df['new_column'] = NewSeries(input_list).to_column()['vals']
 
     return input_df
+
+
+if __name__ == "__main__":
+    input_list= [10, 11, 12]
+    input_df= pandas.DataFrame({'col1': [1, 2, 3],
+                                'col2': [4, 5, 6]})
+
+    print(list_to_column(input_list, input_df))
