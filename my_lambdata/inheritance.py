3dcf7bb6e2809dcc643228aa709167e6bfdae2a7
Steven Chase <stevenchase@Stevens-MacBook-Pro.local>
2020-05-06 14:08:23 -0400

    Added comments to my pages. Added two examples from lectures.

---
+++ my_lambdata/inheritance.py
@@ -0,0 +1,49 @@
+# my_lambdata/inheritance.py
+
+# A class example of using inheritance
+
+from pandas import DataFrame
+
+# Passing in the class DataFrame as this is an inheritance example,
+# Out class will inherit everything from its parent class and then we will modify it
+class WrangledFrame(DataFrame):
+    '''
+    A custom pandas.DataFrame with a column called "abbrev"
+
+    Example: WrangledFrame({"abbrev": ["CA", "CO", "CT", "DC", "TX"]})
+    '''
+
+    def add_state_names(self):
+        '''
+        Converts a dataframe with a column of state abbreviations, adding a corresponding column of state names.
+
+        Returns: a pandas.DataFrame with the original col as well as a "name" column.
+
+        # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.map.html
+        '''
+
+        # need a list or dict with the abbrev/name mappings
+        names_map = {'CA': 'Cali', 'CO': 'Colo', 
+                    'CT': 'Conn', 'DC': 'District of Columbia'}
+
+        # create a new column which maps the existing column using our names map
+        self['name'] = self['abbrev'].map(names_map)
+
+    def inspect_columns(self):
+        print(self.columns)
+
+
+if __name__ == "__main__":
+
+# Since we are inheriting from the DataFrame class we can use its methods: ex df.head()
+
+# Instantiate a new object using our class
+    wf = WrangledFrame({'abbrev': ['CA', 'CO', 'CT', 'DC', 'TX']})
+    print(wf.head())
+
+# Call the add state names method to our new class
+    wf.add_state_names()
+    print(wf.head())
+
+# Call the inspect method to our new class
+    wf.inspect_columns
--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -1,15 +1,18 @@
 import pandas
 
+# Import package I created on my_mod.py page
 from my_lambdata.my_mod import list_to_column
 
-# print("Hello World")
+# Testing working with terminal commands
+print("Hello World")
 
-# print(2+2)
+print(2+2)
 
+# Testing calling mod and inputing values
 input_df = pandas.DataFrame({"a":[1,2,3], "b":[4,5,6]})
 
 input_list= [7, 8, 9]
 
-# print(list_to_column(input_list, input_df))
+print(list_to_column(input_list, input_df))
 
 print(input_df['a'])
